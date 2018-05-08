82f1c4116bc2ade2dac2373361060c1f2c6f0ea0
Simon Heinke <simon.heinke@tu-ilmenau.de>
2018-05-08 22:52:26 +0200

    [MA-6] Implemented signals for newly loaded models (in AnalyzeData)

--- applications/mne_analyze/extensions/music/music.cpp
+++ applications/mne_analyze/extensions/music/music.cpp
@@ -186,6 +186,14 @@ QVector<EVENT_TYPE> Music::getEventSubscriptions(void) const
 
 //*************************************************************************************************************
 
+void Music::onNewModelAvailable(QSharedPointer<AbstractModel> model)
+{
+
+}
+
+
+//*************************************************************************************************************
+
 void Music::calculate()
 {
     QString fwdFileOption(QCoreApplication::applicationDirPath() + "/MNE-sample-data/MEG/sample/sample_audvis-meg-eeg-oct-6-fwd.fif");
