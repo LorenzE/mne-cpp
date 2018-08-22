f9f6c630bbf144bc87fbc5905c14e3f8f924edb7
Simon Heinke <simon.heinke@tu-ilmenau.de>
2018-08-22 12:22:16 +0200

    [MA-8] Beautifying MNEAnalyze. Minimum and Maximum width of Extension dock widgets are now defined in the .ui files.

--- applications/mne_analyze/extensions/music/music.cpp
+++ applications/mne_analyze/extensions/music/music.cpp
@@ -153,7 +153,6 @@ QDockWidget *Music::getControl()
     if(!m_pControl) {
         m_pControl = new QDockWidget(tr("MUSIC"));
         m_pControl->setAllowedAreas(Qt::LeftDockWidgetArea | Qt::RightDockWidgetArea);
-        m_pControl->setMinimumWidth(180);
         m_pControl->setWidget(m_pMusicControl);
     }
 
